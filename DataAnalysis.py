eecff43ecc08b59ec72d966cc40f18ef849c2c3e
alex-870 <64564931+alex-870@users.noreply.github.com>
2023-11-30 15:41:11 -0500

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -3,11 +3,15 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from datetime import datetime
+import pandas as pd
 
 
 # change this to log file name
 file_name = 'Log-2023-11-20-19-36-40.csv'
 #file_name = input('Enter log file name: ')
+
+def setColor(a):
+    return 'red'
 
 # opens csv file and creates lists for each column
 with open(file_name) as file:
@@ -19,6 +23,7 @@ with open(file_name) as file:
     gps_alt = []
     max_speed = [0]
     max_index = 0
+    speed2 = []
 
     for count, line in enumerate(data):
         if count > 1:
@@ -29,12 +34,16 @@ with open(file_name) as file:
             if line[1] != 'nan':
                 gps_lat.append(float(line[3]))
                 gps_long.append(float(line[4]))
+                speed2.append(float(line[1]))
             if float(line[1]) > max_speed[0]:
                 max_speed[0] = float(line[1])
-                max_index = int(count)
-            
-# print(max_speed)
-# print(max_index)
+                max_index = int(count)  
+
+
+data_dict = {'Speed (mph)':speed2,
+             'gps_long':gps_long,
+             'gps_lat':gps_lat}
+df = pd.DataFrame(data_dict)
 
 # creates plot for speed and altitude
 fig1 = make_subplots(specs=[[{"secondary_y": True}]])
@@ -53,30 +62,28 @@ fig1.add_trace(
     go.Scatter(x=times[max_index-1:max_index], 
                y=speeds[max_index-1:max_index], 
                name="Max Speed",
-               text='Max Speed',
+               mode="markers+text",
+               text=[f'Max Speed: {round(max_speed[0],2)} mph'],
                textposition='top center'),
     secondary_y=False,
 )
 
-fig1.update_layout(title_text="Speed and Altitude")
-fig1.update_xaxes(title_text="Time")
+fig1.update_layout(title_text="<b>Speed and Altitude</b>")
+fig1.update_xaxes(title_text="<b>Time</b>")
 fig1.update_yaxes(title_text="<b>Speed (mph)</b>", secondary_y=False)
 fig1.update_yaxes(title_text="<b>Altitude (ft)</b>", secondary_y=True)
+fig1.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=.9
+))
 fig1.show()
 
-
 # creates plot for location
-fig2 = px.scatter(x=gps_long,y=gps_lat)
-fig2.update_layout(title_text="Location")
-fig2.update_layout(xaxis_title="Longitude", yaxis_title="Latitude")
+fig2 = px.scatter(df,x='gps_long',y='gps_lat',color='Speed (mph)')
+fig2.update_layout(title_text="<b>Location</b>",
+                   xaxis_title="<b>Longitude</b>",
+                   yaxis_title="<b>Latitude</b>")
 fig2.show()
-
-
-# fig1 = px.line(x=times,y=speeds,title='Speed')
-# fig1.update_layout(xaxis_title="Time", yaxis_title="Speed (mph)")
-# fig1.show()
-
-
-# fig3 = px.line(x=times,y=gps_alt,title='Altitude')
-# fig3.update_layout(xaxis_title="Time", yaxis_title="Altitude (ft)")
-# fig3.show()
